784418eab9f904d1b792b892e2ca97ec8b5c14b9
Alberto Benatti <albi.benni8@gmail.com>
2026-05-21 20:57:53 +0200

    init tests

---
+++ tests/integration_test.rs
@@ -0,0 +1,6 @@
+#[test]
+fn test_app_initialization() {
+    // This is a placeholder test. 
+    // Testing GTK4 applications in a headless CI environment requires a virtual display.
+    assert!(true);
+}
